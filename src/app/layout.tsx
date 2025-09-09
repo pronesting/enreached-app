02944e711e491547164cead9392092be0f7affd9
Maor Sabag <hello@pronesting.com>
2025-09-09 14:55:43 +0300

    AddHotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,21 @@ export default function RootLayout({
         <link rel="icon" href="/Favicon to vercel.png" type="image/png" />
         <link rel="apple-touch-icon" href="/Favicon to vercel.png" />
         <meta name="theme-color" content="#1976d2" />
+        {/* Hotjar Tracking Code for https://www.enreached.co/ */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6515309,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${urbanist.variable} antialiased`}
