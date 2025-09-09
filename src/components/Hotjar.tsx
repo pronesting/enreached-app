304c2bf793720b8479ae46aa585641e3da894bd8
Maor Sabag <hello@pronesting.com>
2025-09-09 15:01:31 +0300

    UpdateHotjarID

--- src/components/Hotjar.tsx
+++ src/components/Hotjar.tsx
@@ -4,12 +4,12 @@ import { useEffect } from 'react';
 
 export function Hotjar() {
   useEffect(() => {
-    // Hotjar Tracking Code for https://www.enreached.co/
+    // Hotjar Tracking Code for enreached app
     (function(h: any, o: any, t: any, j: any, a: any, r: any) {
       h.hj = h.hj || function() {
         (h.hj.q = h.hj.q || []).push(arguments);
       };
-      h._hjSettings = { hjid: 6515309, hjsv: 6 };
+      h._hjSettings = { hjid: 6515321, hjsv: 6 };
       a = o.getElementsByTagName('head')[0];
       r = o.createElement('script');
       r.async = 1;
